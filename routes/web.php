74c1694dc98a1db38c7fae59d1acac29f70b36ee
Aboubacar BANE <aboubacarbane@gmail.com>
2024-02-22 23:51:59 +0000

    mise à jour

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+ 
 Route::get('/', function () {
     return redirect('/login');
 });
